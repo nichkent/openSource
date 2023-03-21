f0105322de20ff6ea5907c6571f703753855744f
Jamoeoe <112401080+Jamoeoe@users.noreply.github.com>
2023-03-21 10:21:44 -0400

    Update datetime_convert.cc
    added comments

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -3,8 +3,10 @@
 #include <sstream>
 #include <chrono>
 
+//outputs formatted date
 int main()
 {
+    //default date
     std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
     std::istringstream ss(date_str);
@@ -13,6 +15,7 @@ int main()
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
+    //outputs formatted date
     std::cout << formatted_date << std::endl;
 
     return 0;
